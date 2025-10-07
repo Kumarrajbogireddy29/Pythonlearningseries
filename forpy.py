2e824f45d24b93b0444e24b70a03619f5f1cfa1b
Kumar Raj Bogireddy <kumarrajbogireddy29@gmail.com>
2025-10-07 19:36:50 +0530

    Add files via upload

---
+++ forpy.py
@@ -0,0 +1,28 @@
+##a,b=map(int,input().split()) # 5 9
+##if a<b:
+##   for i in range(a,b+1):
+##        print(i,end=' ')
+##else :
+##   for i in range(a,b-1,-1):
+##        print(i,end=' ')
+        
+##for i in range(1,6):
+##    print(i,"*",5,5*i)
+##
+##for i in range(1,11):
+##    if i==5:
+##        break
+##    print(i,end=' ')
+##
+##print()
+##print("HI")
+##
+##
+
+for i in range(1,11): # 1 2 3 4 5
+    if i%2==0:
+        continue
+    print(i,end=' ')
+
+print()
+print('hi') 
